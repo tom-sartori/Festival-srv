c97241dccab6aaf7243de5d9011028d0a752441d
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-01-31 20:34:39 +0100

    MongoDb link and sample crud.

---
+++ Festival-srv/src/main/java/festival/srv/constant/MongoDbConstants.java
@@ -0,0 +1,14 @@
+package festival.srv.constant;
+
+public final class MongoDbConstants {
+
+	private MongoDbConstants() { }
+
+
+	public static final String ID = "_id";
+	public static final String NAME = "name";
+	public static final String DESCRIPTION = "description";
+
+	public static final String DATABASE_NAME = "Festival-db";
+	public static final String FRUITS_COLLECTION = "fruits";
+}
--- Festival-srv/src/test/java/festival/srv/getting/started/GreetingResourceTest.java
+++ Festival-srv/src/test/java/festival/srv/getting/started/GreetingResourceTest.java
@@ -4,18 +4,26 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+//    @Test
+//    public void testHelloEndpoint() {
+//        given()
+//          .when().get("/hello")
+//          .then()
+//             .statusCode(200)
+//             .body(is("Hello RESTEasy"));
+//    }
 
+//	@Test
+//	public void testHelloEndpoint() {
+//		given()
+//				.when().get("/fruits")
+//				.then()
+//				.statusCode(200);
+////				.body(is("Hello RESTEasy"));
+//	}
 }

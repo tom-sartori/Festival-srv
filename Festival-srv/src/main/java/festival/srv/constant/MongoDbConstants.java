3a6af61ce625c74d207f5d278155a7a9fd517b25
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-01-31 20:46:12 +0100

    Volunteer CRUD.

--- Festival-srv/src/main/java/festival/srv/constant/MongoDbConstants.java
+++ Festival-srv/src/main/java/festival/srv/constant/MongoDbConstants.java
@@ -8,7 +8,11 @@ public final class MongoDbConstants {
 	public static final String ID = "_id";
 	public static final String NAME = "name";
 	public static final String DESCRIPTION = "description";
+	public static final String FIRST_NAME = "firstName";
+	public static final String LAST_NAME = "lastName";
+	public static final String EMAIL = "email";
 
 	public static final String DATABASE_NAME = "Festival-db";
 	public static final String FRUITS_COLLECTION = "fruits";
+	public static final String VOLUNTEERS_COLLECTION = "volunteers";
 }
---
+++ Festival-srv/src/main/java/festival/srv/entity/Volunteer.java
@@ -0,0 +1,70 @@
+package festival.srv.entity;
+
+import org.bson.Document;
+
+import java.util.Objects;
+
+import static festival.srv.constant.MongoDbConstants.*;
+
+public class Volunteer implements Entity {
+
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String email;
+
+	public Volunteer() { }
+
+	public Volunteer(Document document) {
+		this.id = document.getObjectId(ID).toString();
+		this.firstName = document.getString(FIRST_NAME);
+		this.lastName = document.getString(LAST_NAME);
+		this.email = document.getString(EMAIL);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Volunteer volunteer = (Volunteer) o;
+		return Objects.equals(id, volunteer.id) && Objects.equals(firstName, volunteer.firstName) && Objects.equals(lastName, volunteer.lastName) && Objects.equals(email, volunteer.email);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, firstName, lastName, email);
+	}
+}

6f205e219d0ff232476e2763393ba2baf6cd31bc
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-02-22 14:26:56 +0100

    Jwt generation.

--- src/main/java/festival/srv/constant/ApiPaths.java
+++ src/main/java/festival/srv/constant/ApiPaths.java
@@ -5,6 +5,7 @@ public final class ApiPaths {
 	private ApiPaths() { }
 
 	public static final String FRUIT = "/fruit";
+	public static final String JWT = "/jwt";
 	public static final String USER = "/user";
 	public static final String REGISTER = "/register";
 	public static final String LOGIN = "/login";
